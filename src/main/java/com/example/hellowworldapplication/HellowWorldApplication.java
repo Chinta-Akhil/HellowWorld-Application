1e52a004226fd66d606bd745d2f3435e7eb972f8
Chinta-Akhil <chintaakhil2@gmail.com>
2021-07-29 05:05:31 -0400

    Updated HellowWorldApplication.java

--- src/main/java/com/example/hellowworldapplication/HellowWorldApplication.java
+++ src/main/java/com/example/hellowworldapplication/HellowWorldApplication.java
@@ -6,23 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RequestMapping("/")
 @RestController
+@RequestMapping("/")
+@SpringBootApplication
 
 public class HellowWorldApplication {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/Akhil")
+    public String getAkhil() {
+        return "Hello Akhil!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellowWorldApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
-    }
 
-    @GetMapping("/hello1")
-    public String sayHello1() {
-        return "Hello Akhil!";
-    }
 }
