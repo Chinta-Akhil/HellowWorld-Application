462e97b9d75d33f260c99d968af372d8b2798db4
Chinta <akhil.chinta@capgemini.com>
2021-07-29 04:11:54 -0400

    first commit

---
+++ src/main/java/com/example/hellowworldapplication/HellowWorldApplication.java
@@ -0,0 +1,23 @@
+package com.example.hellowworldapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RequestMapping("/")
+@RestController
+
+public class HellowWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowWorldApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/example/hellowworldapplication/HellowWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowworldapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
